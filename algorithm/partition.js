bcab72908f251a99b97dc0982f56dfa456e80b96
aFish <tako0903@icloud.com>
2018-07-24 09:49:22 +0800

    partition

---
+++ algorithm/partition.js
@@ -0,0 +1,16 @@
+function pratition(n,m) {
+    if ( n == 1 || m == 1 )
+        return 1;
+
+    if ( n==m )
+    	return 1+pratition(n,n-1);
+    
+    if ( n>m )
+        return pratition(n-m,m)+pratition(n,m-1);
+
+    if ( n<m )
+        return pratition(n,n);
+}
+
+let n = 5
+console.log(pratition(n,n));
